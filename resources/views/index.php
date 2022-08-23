60863b5e9db5336cfee5f567cccfb711205ed70a
Zidane071 <zidanesanjaya81@Gmail.com>
2022-08-23 19:44:38 +0700

    Update index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -9,10 +9,9 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="{{ asset('css/style.css') }}>
-<link rel="stylesheet" href="{{ asset ('js/index.js') }}">
-<!-- <link rel="stylesheet" href="asset/css/index.css">
-<link rel="stylesheet" href="asset/js/index.js"> -->
+<link href="/css/index.css" rel="stylesheet">
+<link href="/js/index.js" rel="stylesheet">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   
