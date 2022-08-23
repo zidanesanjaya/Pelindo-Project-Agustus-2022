07d3efbe70188c801593eee0971d5eb257114729
Zidane071 <zidanesanjaya81@webmail.umm.ac.id>
2022-08-23 17:54:42 +0700

    .

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,13 +4,15 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Budi's Project</title>
+<title>Budi's Projects</title>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="asset/css/index.css">
-<link rel="stylesheet" href="asset/js/index.js">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}>
+<link rel="stylesheet" href="{{ asset ('js/index.js') }}">
+<!-- <link rel="stylesheet" href="asset/css/index.css">
+<link rel="stylesheet" href="asset/js/index.js"> -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   
