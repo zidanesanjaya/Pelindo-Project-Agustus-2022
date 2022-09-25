e1fd4f6ab46cdb979d703e4bae3252689488668c
Zidane071 <zidanesanjaya81@Gmail.com>
2022-09-25 21:50:43 +0700

    z

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand">
+                <a class="navbar-brand" href="{{ url ('http://127.0.0.1:8000/') }}">
                     Admin
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
